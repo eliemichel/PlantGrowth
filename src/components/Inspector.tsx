13873c7c44b9d77ffeaf1068b823ca8b08feac48
Elie Michel <elie.michel@exppad.com>
2024-08-09 00:03:19 +0200

    Add inspector

--- src/components/Inspector.tsx
+++ src/components/Inspector.tsx
@@ -1,20 +1,35 @@
-import { useMemo } from 'react'
 import { useScene } from '../reducers/sceneReducer.tsx'
+import './Inspector.css'
 
 export default function Inspector() {
 	const scene = useScene();
 	const branches = scene.branches;
 
-	const phytomerCount = useMemo(
-		() => branches.reduce((acc, branch) => acc + branch.points.length - 1, 0),
-		[ branches ]
-	);
-
 	return (
 		<>
-			<p>
-				Phytomer Count: <strong>{phytomerCount}</strong>
-			</p>
+			<h3>
+			Branches
+			</h3>
+			<table className="spreadsheet">
+				<thead>
+					<tr>
+						<th>id</th>
+						<th>points</th>
+						<th>leaves</th>
+						<th>buds</th>
+					</tr>
+				</thead>
+				<tbody>
+					{branches.map((b, idx) => (
+						<tr>
+							<td>#{idx}</td>
+							<td>{b.points.length}</td>
+							<td>{b.leaves.length}</td>
+							<td>{b.buds.length}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	)
 }
