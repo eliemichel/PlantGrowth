71e57b9dac295c2d49a85807b187f17c5d66d5c2
Elie Michel <elie.michel@exppad.com>
2024-08-10 17:00:30 +0200

    Split up applyBehavior

--- src/reducers/behaviors.tsx
+++ src/reducers/behaviors.tsx
@@ -16,29 +16,166 @@ import {
 
 import { Vector, addInPlace, add, copyVector } from '../utils/vector.tsx'
 
+/* ********** Behavior declarations ********** */
+
+/**
+ * Organogenesis does not move any existing nodes, but it may create new
+ * elements in branches or even new branches.
+ */
+export type OrganogenesisBehavior = {
+  type: 'organogenesis',
+  handleBranch: (growthModel: GrowthModel, branch: Branch, nextBranchRef: BranchRef) => Branch[],
+}
+
+/**
+ * Continuous growth only moves existing nodes. It can move internal nodes,
+ * which has a recursive effect on all subsequent nodes. This returns for
+ * each node a position update expressed in its local growth frame. A node is
+ * identified by its branch + node index. The node position is the branch's
+ * points of index nodeIndex + 1 because the first points (the anchor) does
+ * not count as a node (it already does in the parent branch).
+ * TODO: Express the first point differently, as a reference to the parent
+ * branch node.
+ */
+export type GrowthBehavior = {
+  type: 'growth',
+  handleNode: (growthModel: GrowthModel, branch: Branch, nodeIndex: number) => Vector,
+  handleLeaf: (growthModel: GrowthModel, branch: Branch, leafIndex: number) => Leaf,
+}
+
 /**
  * There are different kinds of simulation model updates
  */
 export type Behavior =
-  // Organogenesis does not move any existing nodes, but it may create new
-  // elements in branches or even new branches.
-  | {
-    type: 'organogenesis',
-    handleBranch: (growthModel: GrowthModel, branch: Branch, nextBranchRef: BranchRef) => Branch[],
+  | OrganogenesisBehavior
+  | GrowthBehavior
+
+/* ********** Behavior implementations ********** */
+
+export function applyOrganogenesisBehavior(
+  state: SimulationModel,
+  behavior: OrganogenesisBehavior,
+  /* options */ { repeat = 1 }: { repeat: number }
+): SimulationModel {
+  const { handleBranch } = behavior;
+  // Map the branch handler on all branches, reduces resulting lists together
+  let nextBranches = state.branches;
+  for (let i = 0 ; i < repeat ; ++i) {
+    // Cannot use this nice functional approach because of the temporary
+    // poor man's reference management
+    /*
+    nextBranches = concatAll(nextBranches.map(b => {
+      const growthModel = state.growthModels[b.growthModelIndex];
+      return handleBranch(growthModel, b);
+    }));
+    */
+    const branches = nextBranches;
+    const newBranches: Branch[] = []; // branches that we append at the end
+    nextBranches = branches.map(b => {
+      const growthModel = state.growthModels[b.growthModelIndex];
+      const nextBranchRef = branches.length + newBranches.length;
+      const bb = handleBranch(growthModel, b, nextBranchRef);
+      // We do not handle removing branches yet
+      console.assert(bb.length > 0);
+      // Existing branches must not move in the array not to mess up with
+      // indices, so the first element returned by handleBranch is pushed
+      // now, the other ones (newly created branches) are kept for the end.
+      newBranches.push(...bb.slice(1));
+      return bb[0];
+    })
+    nextBranches.push(...newBranches);
   }
-  // Continuous growth only moves existing nodes. It can move internal nodes,
-  // which has a recursive effect on all subsequent nodes. This returns for
-  // each node a position update expressed in its local growth frame. A node is
-  // identified by its branch + node index. The node position is the branch's
-  // points of index nodeIndex + 1 because the first points (the anchor) does
-  // not count as a node (it already does in the parent branch).
-  // TODO: Express the first point differently, as a reference to the parent
-  // branch node.
-  | {
-    type: 'growth',
-    handleNode: (growthModel: GrowthModel, branch: Branch, nodeIndex: number) => Vector,
-    handleLeaf: (growthModel: GrowthModel, branch: Branch, leafIndex: number) => Leaf,
+  return {
+    ...state,
+    branches: nextBranches,
+  };
+}
+
+/**
+ * TODO: Find a way to signal the Viewport that only positions moved, but
+ * the structure remains the same. Modying state in place is not an option
+ * because React uses double dipspatching in dev mode to ensure
+ * idempotence of action handling.
+ */
+export function applyGrowthBehavior(
+  state: SimulationModel,
+  behavior: GrowthBehavior,
+  /* options */ { repeat = 1 }: { repeat: number }
+): SimulationModel {
+  const { handleNode, handleLeaf } = behavior;
+
+  let branches = state.branches;
+
+  for (let i = 0 ; i < repeat ; ++i) {
+
+    // Allocate memory to store growth vectors for each node
+    const pointUpdates: Vector[][] = branches.map(b => b.points.map(_ => [ 0, 0, 0 ]));
+
+    // Grow from origin to tip so that we accumulate transform
+    for (const plant of state.plants) {
+      // branches to be handled, sorted
+      const fifo: { branchRef: BranchRef, accumulatedOffset: Vector }[] = [];
+
+      fifo.push({
+        branchRef: plant.shoot,
+        accumulatedOffset: [ 0, 0, 0 ],
+      });
+
+      let next;
+      while ((next = fifo.shift()) !== undefined) {
+        const { branchRef, accumulatedOffset } = next;
+        console.assert(branchRef >= 0 && branchRef < branches.length);
+        const branch = branches[branchRef];
+        const update = pointUpdates[branchRef];
+        const growthModel = state.growthModels[branch.growthModelIndex];
+
+        const newOffset: Vector = [ ...accumulatedOffset ];
+        copyVector(update[0], newOffset);
+
+        for (let nodeIndex = 0 ; nodeIndex < branch.points.length - 1 ; ++nodeIndex) {
+          // Estimate node movement
+          const deltaNodePosition = handleNode(growthModel, branch, nodeIndex);
+
+          // Add to the accumulated offset that gets applied to this node
+          // and all of its children.
+          addInPlace(newOffset, deltaNodePosition);
+
+          // Apply accumulated offset
+          copyVector(update[nodeIndex + 1], newOffset);
+        }
+
+        // We grow leaves directly
+        branch.leaves = branch.leaves.map((_, idx) => handleLeaf(growthModel, branch, idx));
+
+        for (const childRef of branch.children) {
+          fifo.push({
+            branchRef: childRef,
+            accumulatedOffset: [...newOffset],
+          });
+        }
+      }
+    }
+
+    // Apply updates all at once
+    const nextBranches = branches.map((branch, branchIndex) => {
+      const update = pointUpdates[branchIndex];
+      const growthModel = state.growthModels[branch.growthModelIndex];
+      return {
+        ...branch,
+        points: branch.points.map((point, pointIndex) => add(point, update[pointIndex])),
+        leaves: branch.leaves.map((_, leafIndex) => handleLeaf(growthModel, branch, leafIndex)),
+      }
+    });
+
+    branches = nextBranches;
   }
+
+  // Although we modify in place, create new objects to trigger re-render
+  return {
+    ...state,
+    branches,
+  }
+}
 
 /**
  * For a given behavior type, the application of the behavior to the model is
@@ -52,128 +189,18 @@ export type Behavior =
 export function applyBehavior(
   state: SimulationModel,
   behavior: Behavior,
-  /* options */ { repeat = 1 }: { repeat: number }
+  options: { repeat: number }
 ): SimulationModel {
   switch (behavior.type) {
 
-    case "organogenesis": {
-      const { handleBranch } = behavior;
-      // Map the branch handler on all branches, reduces resulting lists together
-      let nextBranches = state.branches;
-      for (let i = 0 ; i < repeat ; ++i) {
-        // Cannot use this nice functional approach because of the temporary
-        // poor man's reference management
-        /*
-        nextBranches = concatAll(nextBranches.map(b => {
-          const growthModel = state.growthModels[b.growthModelIndex];
-          return handleBranch(growthModel, b);
-        }));
-        */
-        const branches = nextBranches;
-        const newBranches: Branch[] = []; // branches that we append at the end
-        nextBranches = branches.map(b => {
-          const growthModel = state.growthModels[b.growthModelIndex];
-          const nextBranchRef = branches.length + newBranches.length;
-          const bb = handleBranch(growthModel, b, nextBranchRef);
-          // We do not handle removing branches yet
-          console.assert(bb.length > 0);
-          // Existing branches must not move in the array not to mess up with
-          // indices, so the first element returned by handleBranch is pushed
-          // now, the other ones (newly created branches) are kept for the end.
-          newBranches.push(...bb.slice(1));
-          return bb[0];
-        })
-        nextBranches.push(...newBranches);
-      }
-      return {
-        ...state,
-        branches: nextBranches,
-      }; 
-    }
+    case "organogenesis":
+      return applyOrganogenesisBehavior(state, behavior, options);
 
-    // TODO: Find a way to signal the Viewport that only positions moved, but
-    // the structure remains the same. Modying state in place is not an option
-    // because React uses double dipspatching in dev mode to ensure
-    // idempotence of action handling.
-    case "growth": {
-      const { handleNode, handleLeaf } = behavior;
+    case "growth":
+      return applyGrowthBehavior(state, behavior, options);
 
-      let branches = state.branches;
-
-      for (let i = 0 ; i < repeat ; ++i) {
-
-        // Allocate memory to store growth vectors for each node
-        const pointUpdates: Vector[][] = branches.map(b => b.points.map(_ => [ 0, 0, 0 ]));
-
-        // Grow from origin to tip so that we accumulate transform
-        for (const plant of state.plants) {
-          // branches to be handled, sorted
-          const fifo: { branchRef: BranchRef, accumulatedOffset: Vector }[] = [];
-
-          fifo.push({
-            branchRef: plant.shoot,
-            accumulatedOffset: [ 0, 0, 0 ],
-          });
-
-          let next;
-          while ((next = fifo.shift()) !== undefined) {
-            const { branchRef, accumulatedOffset } = next;
-            console.assert(branchRef >= 0 && branchRef < branches.length);
-            const branch = branches[branchRef];
-            const update = pointUpdates[branchRef];
-            const growthModel = state.growthModels[branch.growthModelIndex];
-
-            const newOffset: Vector = [ ...accumulatedOffset ];
-            copyVector(update[0], newOffset);
-
-            for (let nodeIndex = 0 ; nodeIndex < branch.points.length - 1 ; ++nodeIndex) {
-              // Estimate node movement
-              const deltaNodePosition = handleNode(growthModel, branch, nodeIndex);
-
-              // Add to the accumulated offset that gets applied to this node
-              // and all of its children.
-              addInPlace(newOffset, deltaNodePosition);
-
-              // Apply accumulated offset
-              copyVector(update[nodeIndex + 1], newOffset);
-            }
-
-            // We grow leaves directly
-            branch.leaves = branch.leaves.map((_, idx) => handleLeaf(growthModel, branch, idx));
-
-            for (const childRef of branch.children) {
-              fifo.push({
-                branchRef: childRef,
-                accumulatedOffset: [...newOffset],
-              });
-            }
-          }
-        }
-
-        // Apply updates all at once
-        const nextBranches = branches.map((branch, branchIndex) => {
-          const update = pointUpdates[branchIndex];
-          const growthModel = state.growthModels[branch.growthModelIndex];
-          return {
-            ...branch,
-            points: branch.points.map((point, pointIndex) => add(point, update[pointIndex])),
-            leaves: branch.leaves.map((_, leafIndex) => handleLeaf(growthModel, branch, leafIndex)),
-          }
-        });
-
-        branches = nextBranches;
-      }
-
-      // Although we modify in place, create new objects to trigger re-render
-      return {
-        ...state,
-        branches,
-      }
-    }
-
-    default: {
+    default:
       throw Error("Unhandled behavior type: " + JSON.stringify(behavior));
-    }
 
   }
 }
